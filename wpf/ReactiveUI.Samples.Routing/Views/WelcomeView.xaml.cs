863dd698c83b83c5bf221ff1df3f284d00865549
Rodney Santiago <GD000007711@ups.com>
2024-05-08 10:02:00 -0400

    wip telerik

--- wpf/ReactiveUI.Samples.Routing/Views/WelcomeView.xaml.cs
+++ wpf/ReactiveUI.Samples.Routing/Views/WelcomeView.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using ReactiveUI.Samples.Routing.ViewModels;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 namespace ReactiveUI.Samples.Routing.Views
 {
@@ -26,6 +27,8 @@ namespace ReactiveUI.Samples.Routing.Views
         public WelcomeView()
         {
             InitializeComponent();
+
+            PopulateData(); 
 
             this.WhenActivated(d =>
             {
@@ -48,5 +51,37 @@ namespace ReactiveUI.Samples.Routing.Views
             get { return ViewModel; }
             set { ViewModel = (IWelcomeViewModel)value; }
         }
+        public void PopulateData()
+        {
+            ObservableCollection<Employee> employees = new();
+            employees.Add(new Employee() { Name = "Rodney01", CompanyName = "Company01", Title = "Title01" });
+            employees.Add(new Employee() { Name = "Rodney02", CompanyName = "Company02", Title = "Title02" });
+            employees.Add(new Employee() { Name = "Rodney03", CompanyName = "Company03", Title = "Title03" });
+            employees.Add(new Employee() { Name = "Rodney04", CompanyName = "Company04", Title = "Title04" });
+            employees.Add(new Employee() { Name = "Rodney05", CompanyName = "Company05", Title = "Title05" });
+
+            employees.Add(new Employee() { Name = "Rodney06", CompanyName = "Company06", Title = "Title06" });
+            employees.Add(new Employee() { Name = "Rodney07", CompanyName = "Company07", Title = "Title07" });
+            employees.Add(new Employee() { Name = "Rodney08", CompanyName = "Company08", Title = "Title08" });
+            employees.Add(new Employee() { Name = "Rodney09", CompanyName = "Company09", Title = "Title09" });
+            employees.Add(new Employee() { Name = "Rodney10", CompanyName = "Company10", Title = "Title10" });
+
+            radDataPager1.Source = employees;
+        }
+
+        public Employee MyProperty { get; set; }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            PopulateData();
+
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string CompanyName { get; set; }
+        public string Title { get; set; }
     }
 }
