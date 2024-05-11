d9bba1f22c370dc6c127621797bc650baaed10a1
Rodney Santiago <rodney@rcsan96.onmicrosoft.com>
2024-05-11 16:01:22 -0400

    practice

--- wpf/ReactiveUI.Samples.Routing/Views/SecondView.xaml.cs
+++ wpf/ReactiveUI.Samples.Routing/Views/SecondView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,17 +13,53 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Telerik.Windows.Controls.VirtualGrid;
 
 namespace ReactiveUI.Samples.Routing.Views
 {
+
+
+    class Customer
+    {
+        public string FirstName { get; set; }
+    }
+    
     /// <summary>
     /// Interaction logic for SecondView.xaml
     /// </summary>
     public partial class SecondView
     {
+        List<Customer> _customers;
         public SecondView()
         {
             InitializeComponent();
+            _customers = new List<Customer>();
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            _customers.Add(new Customer { FirstName = "Rodney" });
+            var customers = _customers;
+
+            this.VirtualGrid.DataProvider = new BaseDataProvider(customers);
+            this.PinNameColumn();
 
             this.WhenActivated(d =>
             {
@@ -30,10 +67,24 @@ namespace ReactiveUI.Samples.Routing.Views
             });
         }
 
+        private void PinNameColumn()
+        {
+            var columnToPin = this.VirtualGrid.DataProvider.ItemProperties.Where(ip => ip.Name == "Contact_Name").FirstOrDefault();
+            var columnToPinIndex = this.VirtualGrid.DataProvider.ItemProperties.IndexOf(columnToPin);
+            this.VirtualGrid.PinColumnLeft(columnToPinIndex);
+        }
+
         private void virtualGrid_CellValueNeeded(object sender,
         Telerik.Windows.Controls.VirtualGrid.CellValueEventArgs e)
         {
             e.Value = String.Format("{0}.{1}", e.RowIndex, e.ColumnIndex);
+        }
+    }
+
+    internal class BaseDataProvider : DataProvider
+    {
+        public BaseDataProvider(IEnumerable source) : base(source)
+        {
         }
     }
 }
